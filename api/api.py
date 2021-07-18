87325b3e81853677338c982cf70897212e6e4924
Anusha Saha <anushasaha16@gmail.com>
2021-07-17 22:02:26 -0500

    modified model

--- api/api.py
+++ api/api.py
@@ -5,12 +5,12 @@ import numpy as np
 import tensorflow as tf
 import keras
 from keras.models import load_model
+from sklearn.preprocessing import StandardScaler
 
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
 
-CONV_WIDTH = 3
 model = load_model('model\model.h5')
 
 @app.route('/api/v1/forecast/', methods=['GET'])
@@ -18,11 +18,14 @@ def api_data():
     input = []
     for i in range(1,7):
         input.append([float(request.args.get(f'input{i}'))])
-    input = tf.stack([np.array(input)])
-    input.set_shape([None, 6, 1])
-    results = model.predict(input, verbose=0)
-    results = np.squeeze(results)
-    results = results.tolist()
-    return jsonify(results)
+    scaler = StandardScaler()
+    input_scaled = scaler.fit_transform(input)
+    input_scaled = tf.stack([np.array(input_scaled)])
+    input_scaled.set_shape([None, 6, 1])
+    results = model.predict(input_scaled, verbose=0)
+    scaled_results = scaler.inverse_transform([results])
+    scaled_results = np.squeeze(scaled_results)
+    scaled_results = scaled_results.tolist()
+    return jsonify(scaled_results)
 
 app.run()
